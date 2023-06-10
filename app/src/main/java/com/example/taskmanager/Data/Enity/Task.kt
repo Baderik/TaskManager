168a8a762ba31c1cf4c6e581f56322d6c3b870f3
baderik <baderikpavel@gmail.com>
2023-06-10 18:41:55 +1000

    add Task entity

--- app/src/main/java/com/example/taskmanager/Data/Enity/Task.kt
+++ app/src/main/java/com/example/taskmanager/Data/Enity/Task.kt
@@ -1,3 +1,15 @@
 package com.example.taskmanager.Data.Enity
 
-data class Task()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity
+data class Task(
+    @PrimaryKey
+    val id: UUID = UUID.randomUUID(),
+    val description: String,
+    val isSuccess: Boolean,
+    val isSelected: Boolean,
+    val mainTask: UUID?
+)
