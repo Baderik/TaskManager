04c56f7ac6ab650183abbf7b5beb72a2d33aa1ac
baderik <baderikpavel@gmail.com>
2023-06-10 20:31:55 +1000

    add database function in database repository

--- app/src/main/java/com/example/taskmanager/Data/Repository/DatabaseRepository.kt
+++ app/src/main/java/com/example/taskmanager/Data/Repository/DatabaseRepository.kt
@@ -1,9 +1,12 @@
 package com.example.taskmanager.Data.Repository
 
 import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.example.taskmanager.Data.Database.TaskManagerDatabase
+import com.example.taskmanager.Data.Enity.Task
 import java.lang.IllegalStateException
+import java.util.UUID
 
 private const val DATABASE_NAME = "task-database"
 
@@ -17,9 +20,30 @@ class DatabaseRepository private constructor(context: Context){
 
     private val taskDao = database.taskManagerDao()
 
+    fun getAllTasks(): LiveData<List<Task>> = taskDao.getAllTasks()
 
+    fun getTask(id: UUID) = taskDao.getTask(id = id)
 
+    fun getSelectedTasks():LiveData<List<Task>> = taskDao.getSelectedTasks()
 
+    fun getSuccessTasks():LiveData<List<Task>> = taskDao.getSuccessTasks()
+
+    fun getSubtasks(mainTaskId: UUID):LiveData<Task?> = taskDao.getSubtask(mainTaskId = mainTaskId)
+
+    suspend fun removeTask(task: Task){
+        taskDao.deleteSubtask(task.id)
+        taskDao.deleteTask(task)
+    }
+
+    suspend fun updateTask(task: Task){
+        taskDao.updateTask(task = task)
+        if (task.isSuccess)
+            taskDao.noteSubtasksAsCompiled(task.id)
+    }
+
+    suspend fun add(task: Task){
+        taskDao.addTask(task)
+    }
 
 
     companion object{
