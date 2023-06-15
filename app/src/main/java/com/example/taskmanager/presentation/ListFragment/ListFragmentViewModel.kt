38a11a3a497996a3000277bf38b5cb9449450d1c
baderik <baderikpavel@gmail.com>
2023-06-15 19:56:38 +1000

    create ListFragmentViewModel

---
+++ app/src/main/java/com/example/taskmanager/presentation/ListFragment/ListFragmentViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.taskmanager.presentation.ListFragment
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.taskmanager.Data.Enity.Task
+import com.example.taskmanager.Data.Repository.DatabaseRepository
+import com.example.taskmanager.Domain.UseCases.GetSelectedTasksUseCase
+import com.example.taskmanager.Domain.UseCases.GetSuccessTasksUseCase
+import com.example.taskmanager.Domain.UseCases.GetTasksUseCase
+import com.example.taskmanager.Domain.UseCases.UpdateSelectedTaskUseCase
+import com.example.taskmanager.Domain.UseCases.UpdateTaskDataUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ListFragmentViewModel:ViewModel() {
+    private val repository = DatabaseRepository.get()
+
+    private val getTask = GetTasksUseCase(repository)
+    private val getSelectedTask = GetSelectedTasksUseCase(repository)
+    private val getSuccessTasks = GetSuccessTasksUseCase(repository)
+    private val updateSelectedTask = UpdateSelectedTaskUseCase(repository)
+    private val updateSuccessTask = UpdateTaskDataUseCase(repository)
+
+    val tasks: LiveData<List<Task>> = getTask.execute()
+    val selectedTasks: LiveData<List<Task>> = getSelectedTask.execute()
+    val successTasks: LiveData<List<Task>> = getSuccessTasks.execute()
+
+    fun isSelectedButton(task: Task) {
+        viewModelScope.launch(Dispatchers.IO) {
+            updateSelectedTask.execute(task)
+        }
+    }
+
+    fun isSuccessButton(task: Task) {
+        viewModelScope.launch(Dispatchers.IO) {
+            updateSuccessTask.execute(task)
+        }
+    }
+}
