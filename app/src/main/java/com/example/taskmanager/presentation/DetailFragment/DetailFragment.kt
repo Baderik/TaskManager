9e3ffd33fd944e6a20e171e7c32b7e1821a58e1e
baderik <baderikpavel@gmail.com>
2023-06-16 16:06:10 +1000

    add subtask creation dialog

--- app/src/main/java/com/example/taskmanager/presentation/DetailFragment/DetailFragment.kt
+++ app/src/main/java/com/example/taskmanager/presentation/DetailFragment/DetailFragment.kt
@@ -2,16 +2,22 @@ package com.example.taskmanager.presentation.DetailFragment
 
 import android.os.Bundle
 import android.renderscript.ScriptGroup.Binding
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import com.example.taskmanager.Data.Enity.Task
+import com.example.taskmanager.R
+import com.example.taskmanager.databinding.BottomSheetAddTaskBinding
 import com.example.taskmanager.databinding.FragmentDetailBinding
 import com.example.taskmanager.databinding.FragmentListBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import java.util.UUID
 
 class DetailFragment:Fragment() {
@@ -21,6 +27,7 @@ class DetailFragment:Fragment() {
     private val detailFragmentViewModel:DetailFragmentViewModel by lazy {
         ViewModelProvider(this)[DetailFragmentViewModel::class.java]
     }
+    private lateinit var subtaskCreationDialog: BottomSheetDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +38,13 @@ class DetailFragment:Fragment() {
 
         val taskId = requireArguments().getSerializable(TASK_ID) as UUID
         detailFragmentViewModel.onCreate(taskId)
+
+        subtaskCreationDialog = BottomSheetDialog(requireContext(), R.style.DialogStyle)
+        subtaskCreationDialog.window?.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        makeSubtaskCreationDialog()
+        binding.addSubtaskButton.setOnClickListener{
+            subtaskCreationDialog.show()
+        }
 
         return binding.root
     }
@@ -50,6 +64,42 @@ class DetailFragment:Fragment() {
             titleEditText.setText(task.title)
             descriptionEditText.setText(task.description)
         }
+    }
+
+    private fun makeSubtaskCreationDialog() {
+        val dialogBinding = BottomSheetAddTaskBinding.inflate(LayoutInflater.from(requireContext()), null, false)
+
+        dialogBinding.saveTaskButton.isEnabled = false
+
+        dialogBinding.titleEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                dialogBinding.saveTaskButton.isEnabled = s?.isNotBlank() == true
+            }
+        })
+
+
+        dialogBinding.saveTaskButton.setOnClickListener {
+            val task = Task(
+                title = dialogBinding.titleEditText.text.toString(),
+                description = dialogBinding.descriptionEditText.text.toString(),
+                isSelected = false,
+                isSuccess = false,
+                mainTaskId = null
+            )
+            subtaskCreationDialog.dismiss()
+        }
+        dialogBinding.titleEditText.requestFocus()
+
+        subtaskCreationDialog.setOnDismissListener {
+            dialogBinding.apply {
+                titleEditText.text.clear()
+                descriptionEditText.text.clear()
+            }
+        }
+
+        subtaskCreationDialog.setContentView(dialogBinding.root)
     }
 
     override fun onDetach() {
